65205449205d37597fc6d82f808feff6e69a4c10
tereshkevich1 <tereshkevich.cergey@mail.ru>
2024-09-22 20:36:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,46 @@
+package com.example.testapp.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.testapp.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+    private lateinit var galleryViewModel: GalleryViewModel
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        setUpViewModel()
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setUpViewModel(){
+        galleryViewModel =
+            ViewModelProvider(this)[GalleryViewModel::class.java]
+    }
+}
